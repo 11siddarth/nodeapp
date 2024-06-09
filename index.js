53a32b24b1ce5ea047e47a83d67596c495e11d68
11siddarth <shettysid16@gmail.com>
2024-06-09 22:00:54 +0530

    deploy to ecs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res){
-    res.send("Hello World application to ecs");
+    res.send("Hello World application on ecs fargate");
 });
 
 app.listen(80);
